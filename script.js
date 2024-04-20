27a7719bd89e10112a3e1a10a4eba8556fb2615a
Osman Talha Umucu <osmanumucu40@gmail.com>
2024-04-20 13:16:03 +0200

    finishedfr

--- script.js
+++ script.js
@@ -10,3 +10,9 @@ leftImg.src = diceImage;
 
 var rightImg = document.getElementsByClassName("img2")[0]; // Access the first element with class 'img1'
 rightImg.src = diceImage2;
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 won"
+} else {
+    document.querySelector("h1").innerHTML = "Player 2 won"
+}
