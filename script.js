c01459b80029518ff6f7e1854986f44ace37a67d
Osman Talha Umucu <osmanumucu40@gmail.com>
2024-04-20 13:00:53 +0200

    finished

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-console.log(randomNumber1);
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 var diceImage = "images/dice" + randomNumber1 + ".png";
+var diceImage2 = "images/dice" + randomNumber2 + ".png";
+
 
 var leftImg = document.getElementsByClassName("img1")[0]; // Access the first element with class 'img1'
 leftImg.src = diceImage;
+
+var rightImg = document.getElementsByClassName("img2")[0]; // Access the first element with class 'img1'
+rightImg.src = diceImage2;
